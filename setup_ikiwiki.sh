8d37260b3b44807b1c75717e7d1a98b446ece212
bond <bond@derechoaleer.org>
2021-11-21 21:53:40 +0000

    setup ikiwiki updated

--- setup_ikiwiki.sh
+++ setup_ikiwiki.sh
@@ -24,5 +24,18 @@
      # libtext-multimarkdown-perl (para usar por ej markdown=1 en el html)
 # 1) if param not defined read value from user
 # 2) clone SRCURL git repo in src/
+# 2b) crear bare en src.git
+
+#      git init --bare src.git
+
+#   checkear src.git/config 
+#   [core]
+#   	repositoryformatversion = 0
+#   	filemode = true
+#   	bare = true
+#   	sharedrepository = 1
+#   [receive]
+#  	denyNonFastforwards = true
+
 # 3) replace values in wiki.setup.yaml.template and create wiki.setup.yaml
-# 4) ikiwiki --setup wiki.setup.yaml (hace falta crear manualmente el src.git repo?)
+# 4) ikiwiki --setup wiki.setup.yaml (hace falta crear manualmente el src.git repo? parece que si!)
