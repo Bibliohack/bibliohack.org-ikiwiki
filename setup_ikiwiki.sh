a36b56b569144107792e53d46854d02bb97410bc
bond <bond@derechoaleer.org>
2021-11-29 01:19:48 +0000

    updated setup_ikiwiki.sh

--- setup_ikiwiki.sh
+++ setup_ikiwiki.sh
@@ -12,6 +12,9 @@
 #  DESTDIR </var/www/path/public_html>
 #  URL <http://www.example.com>
 
+#  GIT_REMOTE <git@github.com:Bibliohack/bibliohack.org-contents.git>
+
+
 #  SRCURL <https:/github.com/user/mkdn-content-repo>
 
 # ToDo
@@ -24,7 +27,31 @@
      # libtext-multimarkdown-perl (para usar por ej markdown=1 en el html)
 # 1) if param not defined read value from user
 # 2) clone SRCURL git repo in src/
-# 2b) crear bare en src.git
+#       cd ${IKIDIR}
+# 		git clone ${GIT_REMOTE} src
+
+# 	 !importante, configurar src/.git/config para que apunte al bare local de ikiwiki y no a github!
+
+# [core]
+# 	repositoryformatversion = 0
+# 	filemode = true
+# 	bare = false
+# 	logallrefupdates = true
+# [user]
+# 	name = ${ADMIN_USER}
+# 	email = ${ADMIN_EMAIL}
+# [remote "origin"]
+# 	url = ${IKIDIR}/src.git
+# 	fetch = +refs/heads/*:refs/remotes/origin/*
+# [branch "master"]
+# 	merge = refs/heads/master
+# 	remote = origin
+# [remote "github"]
+# 	url = ${GIT_REMOTE}
+# 	fetch = +refs/heads/*:refs/remotes/origin/*
+
+
+# 2b) crear bare en src.git (ToDo probar si es necesario hacer esto o lo crea solo ikiwiki!)
 
 #      git init --bare src.git
 
@@ -38,7 +65,25 @@
 #  	denyNonFastforwards = true
 
 # 3) replace values in wiki.setup.yaml.template and create wiki.setup.yaml
-# 4) ikiwiki --setup wiki.setup.yaml (hace falta crear manualmente el src.git repo? parece que si!)
+
+# para lenguajes
+# 	translate_master_language: es|Español
+# 	translate_slave_languages:
+# 	  - en|English
+# 	translate_translatable_pages: "posts or field_item(translatable yes) or index
+
+# para habilitar edicion web verificar que esten comentados:
+# 	#lockedit plugin
+# 	#PageSpec controlling which pages are locked
+# 	#locked_pages: '* and !postcomment(*)'
+
+   
+# 4) ikiwiki --setup wiki.setup.yaml
+#    ó
+#    ikiwiki --rebuild --verbose --setup wiki.setup.yaml
+#
+#  ToDo: checkear error inicial de git que da ikiwiki en la primera ejecucion (despues desaparece al volver a ejecutarlo)
+
 # 5) para send_mail.pl: 
       # sudo apt-get install cpanm build-essential
       # sudo cpanm Capture::Tiny 
